cae0661718dc666c73fb5a06c2226d4567e7b648
Rubens1414 <98895114+Rubens1414@users.noreply.github.com>
2022-05-11 15:13:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -67,6 +68,7 @@ ScreenManager:
         icon: "truck-delivery"
         pos_hint: {"center_x": .5, "center_y": .4}
         on_press: root.manager.current = 'Seleccion'
+        on_press: root.prueba()
     
 <SeleccionScreen>:
     name: 'Seleccion'
@@ -88,7 +90,8 @@ ScreenManager:
         pos_hint: {"center_x": 0.5, "center_y": 0.7}
         size_hint:(0.15,0.15)
         line_color: 0, 0, 0, 0
-        on_press: root.manager.current = 'Preparacion'    
+        on_press: root.manager.current = 'Preparacion'  
+        on_press: root.Tipofrito('Empanada')  
         Image:
             source:'Imagen/selec_Empanada.png'
             center_x:self.parent.center_x
@@ -98,15 +101,18 @@ ScreenManager:
         size_hint:(0.15,0.15)
         line_color: 0, 0, 0, 0
         on_press: root.manager.current = 'Preparacion'
+        on_press: root.Tipofrito('Arepa') 
         Image:
             source:'Imagen/selec_arepa.png'
             center_x:self.parent.center_x
-            center_y:self.parent.center_y     
+            center_y:self.parent.center_y  
+           
     MDRectangleFlatButton:
         pos_hint: {"center_x": 0.5, "center_y": 0.3}
         size_hint:(0.15,0.15)
         line_color: 0, 0, 0, 0
         on_press: root.manager.current = 'Preparacion'
+        on_press: root.Tipofrito('papa')
         Image:
             source:'Imagen/selec_papa.png'
             center_x:self.parent.center_x
@@ -131,6 +137,7 @@ ScreenManager:
         size_hint:(0.15,0.15)
         line_color: 0, 0, 0, 0
         on_press: root.manager.current = 'Ingrediente'
+        on_press: root.TipoPrepa('Horneado')
         Image:
             source:'Imagen/horneado.png'
             center_x:self.parent.center_x
@@ -140,6 +147,7 @@ ScreenManager:
         size_hint:(0.15,0.15)
         line_color: 0, 0, 0, 0
         on_press: root.manager.current = 'Ingrediente'
+        on_press: root.TipoPrepa('Frito')
         Image:
             source:'Imagen/Frito.png'
             center_x:self.parent.center_x
@@ -159,27 +167,117 @@ ScreenManager:
             source:'Imagen/volver.png'
             center_x:self.parent.center_x
             center_y:self.parent.center_y
-        
+    MDRectangleFlatButton:
+        pos_hint: {"center_x": 0.3, "center_y": 0.7}
+        size_hint:(0.1,0.1)
+        line_color: 0, 0, 0, 0
+        on_press: root.manager.current = 'Ingrediente'
+        Image:
+            source:'Imagen/queso.png'
+            center_x:self.parent.center_x
+            center_y:self.parent.center_y  
+    MDRectangleFlatButton:
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        size_hint:(0.1,0.1)
+        line_color: 0, 0, 0, 0
+        on_press: root.manager.current = 'Ingrediente'
+        Image:
+            source:'Imagen/carne.png'
+            center_x:self.parent.center_x
+            center_y:self.parent.center_y 
+    MDRectangleFlatButton:
+        pos_hint: {"center_x": 0.3, "center_y": 0.3}
+        size_hint:(0.1,0.1)
+        line_color: 0, 0, 0, 0
+        on_press: root.manager.current = 'Ingrediente'
+        Image:
+            source:'Imagen/pollo.png'
+            center_x:self.parent.center_x
+            center_y:self.parent.center_y  
+    MDRectangleFlatButton:
+        pos_hint: {"center_x": 0.7, "center_y": 0.7}
+        size_hint:(0.1,0.1)
+        line_color: 0, 0, 0, 0
+        on_press: root.manager.current = 'Ingrediente'
+        Image:
+            source:'Imagen/cerdo.png'
+            center_x:self.parent.center_x
+            center_y:self.parent.center_y 
+    MDRectangleFlatButton:
+        pos_hint: {"center_x": 0.7, "center_y": 0.3}
+        size_hint:(0.1,0.1)
+        line_color: 0, 0, 0, 0
+        on_press: root.manager.current = 'Ingrediente'
+        Image:
+            source:'Imagen/huevo.png'
+            center_x:self.parent.center_x
+            center_y:self.parent.center_y  
+    
 """
 
+class Frito():
+
+    def tipo(self,select):
+       str: Tipo
+       if select=='papa':
+           Tipo='Papa'
+           return Tipo
+       elif select =='Empanada':
+            Tipo='Empanada'
+       elif select=='Arepa':
+            Tipo='Arepa'
+       self.Tipo = Tipo
+    def preparacion(self,select):
+        str: prepar
+        if select=='Horneado':
+            prepar='Horneado'
+            print(prepar)
+        elif select=='Frito':
+            prepar='Frito'
+            print(prepar)
+        self.prepar = prepar
+
+    def relleno(self):
+        pass
+    def relleno_ad(self):
+        pass      
+    def mostrar(self):
+     print('tipo de Frito: {}'.format(self.Tipo)) 
+     print('tipo de preparacion: {}'.format(self.prepar))
+
+class recibo() :
+     
+    pass      
 
 class MenuScreen(Screen):
-    pass
+    def prueba(self,*args):
+        self.prueba1()
+    def prueba1(self):
+        print('Prueba')
 
+class SeleccionScreen(Screen,Frito):
+     def Tipofrito(self,tipo):
+        self.Tipofrito1(tipo)
+     def Tipofrito1(self,n):
+        t_frito=Frito()
+        t_frito.tipo(n)
+        return n
+       
 
-class SeleccionScreen(Screen):
-    pass
-
-
-class PreparacionScreen(Screen):
-    pass
+class PreparacionScreen(Screen,Frito):
+    def TipoPrepa(self,Prepa):
+        self.TipoPrepa1(Prepa)
+    def TipoPrepa1(self,n):
+        prepara_frito=Frito()
+        prepara_frito.preparacion(n)
+        prepara_frito.mostrar()
+        return n
 
 class IngredientesScreen(Screen):
     pass
 
 
 
-# Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(SeleccionScreen(name='Seleccion'))
@@ -191,6 +289,7 @@ class DemoApp(MDApp):
 
     def build(self):
         screen = Builder.load_string(screen_helper)
+        
         return screen
 if __name__ == '__main__':       
   DemoApp().run()
